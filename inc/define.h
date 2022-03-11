5a73bc079cf6d8c418361072e9b7299b4393f8cb
B-Mugnol <bruno_mugnol@hotmail.com>
2022-03-11 19:30:32 +0100

    added env_path_var macro

---
+++ inc/define.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 18:16:29 by bmugnol-          #+#    #+#             */
+/*   Updated: 2022/03/10 21:03:51 by bmugnol-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_H
+# define DEFINE_H
+
+// Enviroment variable which contains path
+# define ENV_PATH_VAR	"PATH="
+// /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+// Error codes (following <errno.h>'s definition)
+# define EBADF	9	// Bad file descriptor
+# define EINVAL	22	// Invalid argument
+
+#endif
