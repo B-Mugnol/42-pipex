d20483e309ee2393511236560fa41c609c2897d2
B-Mugnol <bruno_mugnol@hotmail.com>
2022-03-17 20:25:28 +0100

    beautify + updated required inclusions + added missing function

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,39 +6,47 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 19:14:13 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/03/11 19:26:10 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/03/17 20:18:23 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>	// access, dup, dup2, execve, fork, pipe, unlink
+# include <unistd.h>	// access, dup, dup2, execve, fork, pipe
 # include <fcntl.h>		// open
-# include <stdlib.h>	// exit, //free, malloc
+# include <stdlib.h>	// exit, malloc
+# include <stdio.h>		// perror
 # include <string.h>	// strerror
 # include <sys/wait.h>	// wait, waitpid
-# include "not_libft.h"
+# include "libft.h"
 # include "define.h"
 
-# include <stdio.h>
 typedef struct s_command
 {
 	char	*pathname;
 	char	**param;
-	int		param_count;
+	int		status;
 }	t_command;
+
+typedef struct s_fd_pair
+{
+	int	fd[2];
+	int	status;
+}	t_fd_pair;
 
 // command.c
 t_command	get_command(char *arg, char *envp[]);
+void		free_command(t_command *cmd);
 
 // verifier.c
-int		param_verifier(const int argc);
-int		path_verifier(t_command *cmd, char ***paths);
-int		pathname_verifier(t_command *cmd);
+int			param_verifier(const int argc);
+int			path_verifier(char **paths);
+int			pathname_verifier(t_command cmd);
 
 // error.c
-void	print_coded_error(char *program, char *err_location, int err_code);
-void	print_custom_error(char *program, char *err_location, char *err_msg);
+void		print_coded_error(char *program, char *err_location, int err_code);
+void		print_custom_error(char *program,
+				char *err_location, char *err_msg);
 
 #endif
