5e7d7c154afda040f5d82a0919970ce788909acb
B-Mugnol <bruno_mugnol@hotmail.com>
2022-03-17 20:24:27 +0100

    added used error codes and bash exit status

--- inc/define.h
+++ inc/define.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 18:16:29 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/03/10 21:03:51 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/03/17 20:24:03 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 
 // Enviroment variable which contains path
 # define ENV_PATH_VAR	"PATH="
-// /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-// Error codes (following <errno.h>'s definition)
-# define EBADF	9	// Bad file descriptor
-# define EINVAL	22	// Invalid argument
+// errno.h error codes
+# define ENOENT		2	// No such file or directory
+# define EACCESS	13	// Permission denied
+# define EINVAL		22	// Invalid argument
+
+// Porgram exit status / bash error codes
+# define CMD_NOT_EXEC	126	// Command invoked cannot execute
+# define CMD_NOT_FOUND	127	// command not found
 
 #endif
