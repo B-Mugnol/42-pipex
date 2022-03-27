f0a1751736d9ce9887a1a05a4e35e84d71259f3f
B-Mugnol <bruno_mugnol@hotmail.com>
2022-03-27 21:54:27 +0200

    now works 100%git add src/pipex.c

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 19:14:40 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/03/27 20:34:22 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/03/27 21:51:44 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 static t_fd_pair	io_file_opener(char *infilename, char *outfilename,
 						int here_doc);
 static int			exec_cmd(int r_fd, int w_fd, t_command cmd, char *envp[]);
-static int			pipex(t_fd_pair iof, t_command *cmd, char *envp[],
-						int cmd_count);
+static int			recursive_pipex(t_fd_pair channel, t_command *cmd,
+						int cmd_count, char *envp[]);
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -31,7 +31,7 @@ int	main(int argc, char *argv[], char *envp[])
 	here_doc = !ft_strncmp(argv[1], "here_doc", ft_strlen(argv[1]));
 	iof = io_file_opener(argv[1], argv[argc - 1], here_doc);
 	cmd = fetch_commands(argc, argv, envp, iof);
-	status = pipex(iof, cmd, envp, argc - 3 - here_doc);
+	status = recursive_pipex(iof, cmd, argc - 3 - here_doc, envp);
 	free_command_vector(argc - 3, &cmd);
 	close_fd_pair(iof);
 	return (status);
@@ -61,75 +61,16 @@ static t_fd_pair	io_file_opener(char *infilename, char *outfilename,
 	return (f);
 }
 
-// static void	pipex(t_fd_pair iof, t_command *cmd, char *envp[], int cmd_count)
-// {
-// 	t_fd_pair	channel[2];
-// 	pid_t		pid;
-
-// 	if (pipe(channel[0].fd))
-// 		print_error_exit("pipex: pipe");
-// 	pid = fork();
-// 	if (pid == -1)
-// 		print_error_exit("pipex: fork");
-// 	if (pid == 0)
-// 	{
-// 		close(channel[0].fd[0]);
-// 		exec_cmd(iof.fd[0], channel[0].fd[1], cmd[0], envp);
-// 	}
-// 	else
-// 	{
-// 		if (waitpid(pid, NULL, 0) == -1)
-// 			perror("pipex: waitpid");
-// 		close(channel[0].fd[1]);
-// 		exec_cmd(channel[0].fd[0], iof.fd[1], cmd[1], envp);
-// 	}
-// }
-
-static int	pipex(t_fd_pair iof, t_command *cmd, char *envp[], int cmd_count)
-{
-	return (recursive_pipex(iof, cmd, cmd_count, envp));
-	// t_fd_pair	channel[2];
-	// pid_t		pid;
-	// //outside function
-	// if (pipe(channel[0].fd))
-	// 	print_error_exit("pipex: pipe");
-	// if (cmd_count > 2 && pipe(channel[1].fd))
-	// {
-	// 	close(channel[0].fd[0]);
-	// 	close(channel[0].fd[1]);
-	// 	print_error_exit("pipex: pipe");
-	// }
-	// //
-	// pid = fork();
-	// if (pid == -1)
-	// 	print_error_exit("pipex: fork");
-	// if (pid == 0)
-	// {
-	// 	close(channel[0].fd[0]);
-	// 	exec_cmd(iof.fd[0], channel[0].fd[1], cmd[0], envp);
-	// }
-	// else
-	// {
-	// 	if (waitpid(pid, NULL, 0) == -1)
-	// 		perror("pipex: waitpid");
-	// 	if (--cmd_count > 1)//
-	// 		// recursive_pipex();
-	// 	close(channel[0].fd[1]);
-	// 	exec_cmd(channel[0].fd[0], iof.fd[1], cmd[1], envp);
-	// }
-}
-
-int	recursive_pipex(t_fd_pair channel, t_command *cmd, int cmd_count, char *envp[])
+static int	recursive_pipex(t_fd_pair channel, t_command *cmd, int cmd_count,
+				char *envp[])
 {
 	pid_t	pid;
 	int		r_fd;
-	int		w_fd;
+	int		o_fd;
 
-	w_fd = channel.fd[1];
+	o_fd = channel.fd[1];
 	r_fd = dup(channel.fd[0]);
 	close_if_valid_fd(channel.fd[0]);
-	// if (r_fd == -1)
-	// 	print_error_exit("pipex: dup");
 	if (pipe(channel.fd))
 		print_error_exit("pipex: pipe");
 	pid = fork();
@@ -137,21 +78,17 @@ int	recursive_pipex(t_fd_pair channel, t_command *cmd, int cmd_count, char *envp
 		print_error_exit("pipex: fork");
 	if (pid == 0)
 	{
-		close(channel.fd[0]); //close read
+		close(channel.fd[0]);
 		return (exec_cmd(r_fd, channel.fd[1], cmd[0], envp));
 	}
-	else
-	{
-		if (waitpid(pid, NULL, 0) == -1)
-			perror("pipex: waitpid");
-		// duplica o pipe-read e fecha o pipe
-		close(channel.fd[1]);
-		channel.fd[1] = w_fd;
-		if (cmd_count > 1)
-			return (recursive_pipex(channel, cmd + 1, cmd_count - 1, envp));
-		if (w_fd != -1)
-			return (exec_cmd(channel.fd[0], w_fd, cmd[0], envp));
-	}
+	if (waitpid(pid, NULL, 0) == -1)
+		perror("pipex: waitpid");
+	close(channel.fd[1]);
+	channel.fd[1] = o_fd;
+	if (cmd_count > 2)
+		return (recursive_pipex(channel, cmd + 1, cmd_count - 1, envp));
+	if (o_fd != -1)
+		return (exec_cmd(channel.fd[0], o_fd, cmd[1], envp));
 	return (EXIT_FAILURE);
 }
 
